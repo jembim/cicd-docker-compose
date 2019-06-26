064af77f27749b2075c71aca40e6cd1e0b536717
root <root@wdcdmzyz22033181.ibmcloud.dst.ibm.com>
2019-06-26 02:16:25 -0500

    Initial Commit

---
+++ platform.secrets.sh
@@ -0,0 +1,22 @@
+# Example file to store all the generated secrets for your ADOP stack
+
+# Username for initial admin user
+export INITIAL_ADMIN_USER="admin"
+
+# Password for initial admin user
+export INITIAL_ADMIN_PASSWORD_PLAIN="admin"
+
+# Password for the Jenkins user
+export PASSWORD_JENKINS="f7418abef1e53573"
+
+# Password for the Gerrit user
+export PASSWORD_GERRIT="78e0398e7a257827"
+
+# Root password for the Sonar and Gerrit MySQL instances
+export PASSWORD_SQL="4d3f509ec1375501"
+
+# Root password for Gitlab admin user
+export PASSWORD_GITLAB="40043bbe7c0c3457"
+
+# Root password for the LDAP server
+export LDAP_PWD="5797c684e4961334" 
