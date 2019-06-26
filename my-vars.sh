580a57afa5339de2234955f4846098ed73bbacca
root <root@wdcdmzyz22033181.ibmcloud.dst.ibm.com>
2019-06-26 02:17:47 -0500

    Removed my-vars.sh

--- my-vars.sh
+++
@@ -1,8 +0,0 @@
-export PUBLIC_IP=169.60.88.164
-export CUSTOM_NETWORK_NAME=docker-network
-export TARGET_HOST=169.60.88.164
-export LOGSTASH_HOST=169.60.88.164
-export INITIAL_ADMIN_USER=admin
-export INITIAL_ADMIN_PASSWORD_PLAIN=admin
-export ADOP_CLI_USER=admin
-export ADOP_CLI_PASSWORD=admin
